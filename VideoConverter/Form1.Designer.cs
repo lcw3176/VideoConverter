694864ad63537ac4a704b7bc2c9ac83cb281eb69
firefly <lcw3176@naver.com>
2020-01-18 16:48:54 +0900

    init commit

---
+++ VideoConverter/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace VideoConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(12, 51);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(282, 27);
+            this.loadButton.TabIndex = 0;
+            this.loadButton.Text = "불러오기";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(12, 84);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(282, 27);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "변환 시작";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(12, 117);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(282, 27);
+            this.stopButton.TabIndex = 2;
+            this.stopButton.Text = "변환 중지";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(282, 19);
+            this.listBox1.TabIndex = 3;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 185);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(282, 27);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 226);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.loadButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "mp4Converter";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_Closing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ VideoConverter/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+using Xabe.FFmpeg;
+using Xabe.FFmpeg.Enums;
+
+namespace VideoConverter
+{
+    public partial class Form1 : Form
+    {
+        // 작업 정지 토큰
+        CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+        public Form1()
+        {
+            InitializeComponent();
+            // 유저의 직접적인 환경변수 등록과정 제외를 위해 설정
+            FFmpeg.ExecutablesPath = Application.StartupPath + @"\ffmpeg";
+            
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            // 정지 버튼
+
+            label1.Text = "작업 중지됨";
+            cancellationTokenSource.Cancel();
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            // 파일 불러오기 버튼
+            // 비디오 확장자만 담아냄
+
+            using(OpenFileDialog open = new OpenFileDialog())
+            {
+                open.RestoreDirectory = true;
+
+                open.Filter = "Video Files |*.mkv;*.avi;*.mp4;*.mpg;*.flv;*.wmv;*.asf;*.asx;*.ogm;*.ogv;*.mov";
+
+                if(open.ShowDialog() == DialogResult.OK)
+                {
+                    string path = open.FileName;
+                    listBox1.Items.Clear();
+                    listBox1.Items.Add(path);
+                }
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            // 시작 버튼
+            // mp4 확장자로만 변환
+
+            string outputPath;
+
+            using (SaveFileDialog save = new SaveFileDialog())
+            {
+                save.Filter = "mp4 Files|*.mp4";
+
+                if (save.ShowDialog() == DialogResult.OK)
+                {
+                    outputPath = save.FileName;
+                    RunConversion(listBox1.Items[0].ToString(), outputPath);
+                }
+            }
+
+        }
+
+        private async void RunConversion(string inputPath, string outputPath)
+        {
+            // 변환 함수
+            // 하드웨어 가속 중 퀵싱크 사용
+            // 인텔 cpu에서만 작동할줄 알았는데 AMD 라이젠 3세대(3600x), 비쉐라(FX8300) 에서도 동작 확인
+            // 왜 되는거징
+
+            var mediaInfo = await MediaInfo.Get(inputPath);
+            var videoStream = mediaInfo.VideoStreams.First();
+            var audioStream = mediaInfo.AudioStreams.First();
+
+            var conversion = Conversion.New()
+                .UseHardwareAcceleration(HardwareAccelerator.qsv, VideoCodec.H264, VideoCodec.Libx264)
+                .AddStream(videoStream)
+                .AddStream(audioStream)
+                .SetOutput(outputPath)
+                .SetOverwriteOutput(true)
+                .UseMultiThread(true)
+                .SetPreset(ConversionPreset.UltraFast);
+
+
+            label1.Text = "작업 진행중...";
+            progressBar1.Value = 0;
+
+            conversion.OnProgress += (sender, args) =>
+            {
+                this.Invoke(new Action(
+                delegate ()
+                {
+                    progressBar1.Value = args.Percent;
+
+                    if (args.Percent == 100)
+                    {
+                        label1.Text = "작업 완료";
+                    }
+
+                }));
+            };
+
+            try
+            {
+                await conversion.Start(cancellationTokenSource.Token);
+            }
+
+            catch
+            {
+                return;
+            }
+
+        }
+
+        private void Form_Closing(object sender, FormClosingEventArgs e)
+        {
+            cancellationTokenSource.Cancel();
+            Dispose();
+            Close();
+        }
+    }
+}
